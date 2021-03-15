18eb4b8781fb04380e34164750086ee8842afab6
seohey <55973041+seohey@users.noreply.github.com>
2021-03-15 23:50:49 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import "./Board";
+import Board from "./Board";
 
 export default function App() {
   return (
@@ -9,54 +9,3 @@ export default function App() {
     </div>
   );
 }
-
-const Board = (props) => {
-  return (
-    <div>
-      <div className="board-row">
-        <Square value="0, 0" />
-        <Square value="0, 1" />
-        <Square value="0, 2" />
-        <Square value="0, 3" />
-        <Square value="0, 4" />
-      </div>
-      <div className="board-row">
-        <Square value="1, 0" />
-        <Square value="1, 1" />
-        <Square value="1, 2" />
-        <Square value="1, 3" />
-        <Square value="1, 4" />
-      </div>
-      <div className="board-row">
-        <Square value="2, 0" />
-        <Square value="2, 1" />
-        <Square value="2, 2" />
-        <Square value="2, 3" />
-        <Square value="2, 4" />
-      </div>
-      <div className="board-row">
-        <Square value="3, 0" />
-        <Square value="3, 1" />
-        <Square value="3, 2" />
-        <Square value="3, 3" />
-        <Square value="3, 4" />
-      </div>
-      <div className="board-row">
-        <Square value="4, 0" />
-        <Square value="4, 1" />
-        <Square value="4, 2" />
-        <Square value="4, 3" />
-        <Square value="4, 4" />
-      </div>
-    </div>
-  );
-};
-
-const Square = (props) => {
-  const abc = "square unselected";
-  return (
-    <button className={abc} onClick={() => console.log(props.value)}>
-      {props.value}
-    </button>
-  );
-};
